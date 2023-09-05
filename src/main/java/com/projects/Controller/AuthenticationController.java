a77b8ae65725ab53b82da70ca92ba212601cc8e8
Kevin Ju <kevjuliow@gmail.com>
2023-09-05 21:17:30 +0700

    Updated Handle Exists Username

--- src/main/java/com/projects/Controller/AuthenticationController.java
+++ src/main/java/com/projects/Controller/AuthenticationController.java
@@ -1,9 +1,12 @@
 package com.projects.Controller;
 
+import com.projects.ExceptionMessage.UsernameExist;
 import com.projects.Models.ApplicationUser;
 import com.projects.Models.RegistrationDTO;
 import com.projects.Service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,7 +17,14 @@ public class AuthenticationController {
     private AuthenticationService authenticationService ;
 
     @PostMapping()
-    public ApplicationUser registerUser (@RequestBody RegistrationDTO body) {
-        return authenticationService.registerUser(body.getUsername(), body.getPassword());
+    public ResponseEntity<?> registerUser (@RequestBody RegistrationDTO body) {
+        try{
+            authenticationService.registerUser(body.getUsername(), body.getPassword());
+            return new ResponseEntity<>("Success Created User \n username : "+body.getUsername()+
+                    "\n password : "+body.getPassword()
+                    , HttpStatus.OK);
+        }catch (UsernameExist err){
+            return new ResponseEntity<>(err.getMessage() , HttpStatus.FORBIDDEN);
+        }
     }
 }
